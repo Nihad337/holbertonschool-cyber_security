44534c3384d5874bed76f1a4d3e49e645bf500aa
Nihad337 <nihadhmdov685@gmail.com>
2026-02-15 10:12:15 +0400

    afafs

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Check if getenforce command exists
-if command -v getenforce >/dev/null 2>&1; then
-    MODE=$(getenforce 2>/dev/null)
-    
-    if [ -z "$MODE" ]; then
-        echo "SELinux status:                 disabled"
-    else
-        echo "SELinux status:                 $MODE"
-    fi
-else
-    echo "SELinux status:                 disabled"
-fi
+getenforce 2>/dev/null || echo "disabled"
